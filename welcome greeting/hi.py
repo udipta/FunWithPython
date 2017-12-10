364e0716472b454399a2a40e664fa3e42b38afd4
udipta <iamudi03@gmail.com>
2017-12-10 16:50:43 +0530

    add connectivity status

--- welcome greeting/hi.py
+++ welcome greeting/hi.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 from gtts import gTTS
 import os
+
+
+def TextToSpeech(msg):
+  tts = gTTS(text=msg, lang='en')
+  tts.save("hi.mp3")
+  os.system("mpg321 -q hi.mp3")
+  os.remove('hi.mp3')
+
+  
+def chkNet():
+	import requests
+	try:
+		response = requests.get("https://www.google.com")
+		return True
+	except requests.ConnectionError:
+		return False
+  
 out = os.popen('date "+%r"').read()
-msg = 'Hi! welcome to jarvis! the time is ' + out[:-5]
-tts = gTTS(text=msg, lang='en')
-tts.save("hi.mp3")
-os.system("mpg321 -q hi.mp3")
-os.remove('hi.mp3')
+usr = os.popen('whoami').read()
+host = os.popen('hostname').read()
+msg = 'Hi' + usr + '! welcome to '+ host +'! the time is ' + out[:-5]
+
+if chkNet():
+  TextToSpeech(msg)
