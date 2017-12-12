ba116852ff9c05b0a3b2a148bb56e834a91779a7
udipta <iamudi03@gmail.com>
2017-12-12 19:39:02 +0530

    reduced execusion time

--- welcome greeting/hi.py
+++ welcome greeting/hi.py
@@ -2,24 +2,15 @@
 from gtts import gTTS
 import os
 
-
 def TextToSpeech(msg):
   tts = gTTS(text=msg, lang='en')
   tts.save("hi.mp3")
   os.system("mpg321 -q hi.mp3")
   os.remove('hi.mp3')
 
-  
-def chkNet():
-	import requests
-	try:
-		response = requests.get("https://www.google.com")
-		return True
-	except requests.ConnectionError:
-		return False
-  
 out = os.popen('date "+%r"').read()
 msg = 'Hi' + os.getlogin() + '! welcome to '+ os.uname()[1] +'! the time is ' + out[:-5]
 
-if chkNet():
+conn = os.popen('timeout 0.11 nm-online').read()
+if conn == '':
   TextToSpeech(msg)
